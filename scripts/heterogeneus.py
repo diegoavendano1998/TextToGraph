0f0f6b4577181ec4ab0d7ad1ce40f13b5cc6188c
Diego AB <diegoavendano1998@gmail.com>
2024-03-22 14:58:14 -0600

    Feat: Add heterogeneus script

---
+++ scripts/heterogeneus.py
@@ -0,0 +1,23 @@
+from text2graphapi.src.Heterogeneous import Heterogeneous
+from prettytable import PrettyTable
+
+import nltk
+nltk.download('omw-1.4')
+
+corpus_docs = [
+    {'id': 1, 'doc': "The sun was shining, making the river look bright and happy."},
+    {'id': 2, 'doc': "Even with the rain, the sun came out a bit, making the wet river shine."}
+]
+
+to_hetero_graph = Heterogeneous(graph_type = 'Graph', 
+    window_size = 20,
+    language = 'en', output_format = 'networkx')
+to_hetero_graph_transformed = to_hetero_graph.transform(corpus_docs)
+
+print("_"*100)
+output_table = PrettyTable(to_hetero_graph_transformed[0].keys())
+
+for hetero_graph_transformed in to_hetero_graph_transformed:
+    output_table.add_row(hetero_graph_transformed.values())
+
+print(output_table)

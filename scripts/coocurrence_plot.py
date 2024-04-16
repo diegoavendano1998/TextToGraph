3fd86f96d4cbf7bf0a65b8cb2445baf8c022dfb3
Diego AB <diegoavendano1998@gmail.com>
2024-04-16 16:51:33 -0600

    Feat: Manage all plot styles on client side

--- scripts/coocurrence_plot.py
+++ scripts/coocurrence_plot.py
@@ -6,7 +6,7 @@ import nltk
 
 nltk.download('omw-1.4')
 
-input_file = "long_example.txt"
+input_file = "short_example.txt"
 file = open("inputs/"+input_file, "r")
 corpus_doc = {"id": 1, "doc": file.read()}
 
@@ -24,10 +24,27 @@ for node in sorted(to_word_coocc_graph_transformed[0].get("nodes",[]), key=lambd
 edge_labels = dict([((nodes_labels[edge_from], nodes_labels[edge_to]), f"freq={frequency.get('freq')}") for edge_from, edge_to, frequency in to_word_coocc_graph_transformed[0].get("edges",[])])
 edge_colors = [int((edge_labels[(nodes_labels[edge_from], nodes_labels[edge_to])]).replace("freq=","")) / 10 for edge_from, edge_to in to_word_coocc_graph_transformed[0]["nx_graph"].edges()]
 PLOT_OPTIONS = {
-    "nodes_colors": nodes_colors,
     "nodes_labels": nodes_labels,
     "edge_labels": edge_labels,
     "edge_colors": edge_colors,
+    "nodes_options": {
+        "node_color": nodes_colors,
+        "node_size": 500
+    },
+    "nodes_labels_options": {
+        "font_size": 7,
+    },
+    "edges_options": {
+        "arrows": True, 
+        "arrowsize": 5, 
+        "arrowstyle": '->',
+        "connectionstyle": f"arc3,rad=0.1",
+        # connectionstyle=f"angle3,angleA=90,angleB=0",
+        "edge_color": edge_colors
+    },
+    "edges_labels_options": {
+        "font_size": 5
+    }
 }
 
 to_word_coocc_graph.plot_graph(to_word_coocc_graph_transformed[0]["nx_graph"], "outputs/" + input_file.replace("txt","png"), PLOT_OPTIONS)
